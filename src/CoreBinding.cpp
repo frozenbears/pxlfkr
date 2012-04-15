3d7834f764df011f6c86d401539fbe54d458b396
Marc Sciglimpaglia <frozenbears@gmail.com>
2012-04-14 18:25:23 -0700

    CoreBinding updates

--- src/CoreBinding.cpp
+++ src/CoreBinding.cpp
@@ -3,8 +3,10 @@
 
 #include "Drawing.tolua.h"
 #include "Text.tolua.h"
+#include "FrameBuffer.tolua.h"
 
 void CoreBindings::open(lua_State *L) {
 	tolua_Drawing_open(L);
 	tolua_Text_open(L);
+	tolua_FrameBuffer_open(L);
 }
