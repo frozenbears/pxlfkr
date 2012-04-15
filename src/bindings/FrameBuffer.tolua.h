0ae672e019c682992d478ac40866c78e27d6094e
Marc Sciglimpaglia <frozenbears@gmail.com>
2012-04-14 18:24:47 -0700

    FrameBuffer

---
+++ src/bindings/FrameBuffer.tolua.h
@@ -0,0 +1,8 @@
+/*
+** Lua binding: FrameBuffer
+** Generated automatically by tolua++-1.0.92 on Sun Apr  8 21:29:10 2012.
+*/
+
+/* Exported function */
+TOLUA_API int  tolua_FrameBuffer_open (lua_State* tolua_S);
+
