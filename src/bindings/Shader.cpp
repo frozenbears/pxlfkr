e9192e0c6b4ddce50ff59617ffbae22cbe7c7fd8
Marc Sciglimpaglia <frozenbears@gmail.com>
2012-12-21 16:10:22 -0800

    shader bindings

---
+++ src/bindings/Shader.cpp
@@ -0,0 +1,53 @@
+
+#include "Shader.h"
+#include "cinder/app/AppBasic.h"
+
+using namespace std;
+using namespace ci;
+using namespace ci::app;
+
+Shader::Shader(const string& vertexFile, const string& fragmentFile) {
+	
+	try {
+		mShader = gl::GlslProg(loadResource(vertexFile), loadResource(fragmentFile));
+	} catch( gl::GlslProgCompileExc &exc ) {
+		std::cout << "Shader compile error: " << std::endl;
+		std::cout << exc.what();
+	}
+	  catch( ... ) {
+		std::cout << "Unable to load shader" << std::endl;
+	}
+}
+
+void Shader::bind() {
+	mShader.bind();
+}
+
+void Shader::unbind() {
+	mShader.unbind();
+}
+
+void Shader::uniform1f(string& name, float data) {
+	mShader.uniform(name, data);
+}
+
+void Shader::uniform2f(string& name, float data, float data2) {
+	mShader.uniform(name, Vec2f(data, data2));
+}
+
+void Shader::uniform3f(string& name, float data, float data2, float data3) {
+	mShader.uniform(name, Vec3f(data, data2, data3));
+}
+
+void Shader::uniform4f(string& name, float data, float data2, float data3, float data4) {
+	mShader.uniform(name, Vec4f(data, data2, data3));
+}
+
+void Shader::uniform1i(string& name, int data) {
+	mShader.uniform(name, data);
+}
+
+void Shader::uniform2i(string& name, int data, int data2) {
+	mShader.uniform(name, Vec2i(data, data2));
+}
+
---
+++ src/bindings/Shader.h
@@ -0,0 +1,34 @@
+
+#include "cinder/gl/gl.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/gl/GlslProg.h"
+
+class Shader {
+
+public:
+	Shader();
+	Shader(const std::string& vertexFile, const std::string& fragmentFile);
+	~Shader();
+	
+	void bind();
+	void unbind();
+	
+	/* note to self - the below feels a bit weird, seeing as though we're
+	 converting these flat parameter lists into templated c++ typedefs only
+	 for them to be converted back again underlyingly in the openGL calls.
+	 
+	 to me this suggests we might be better off binding Vec/Color/Mat types
+	 directly, but maybe it's fine?
+	 */
+	
+	void uniform1f(std::string& name, float data);
+	void uniform2f(std::string& name, float data, float data2);
+	void uniform3f(std::string& name, float data, float data2, float data3);
+	void uniform4f(std::string& name, float data, float data2, float data3, float data4);
+	
+	void uniform1i(std::string& name, int data);
+	void uniform2i(std::string& name, int data, int data2);
+private:
+	ci::gl::Texture mTexture;
+	ci::gl::GlslProg mShader;
+};
