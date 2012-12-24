bfc6bd945dce380436ddaedb21c523d9c9dedbcd
Marc Sciglimpaglia <frozenbears@gmail.com>
2012-12-24 14:01:32 -0800

    MoviePlayer lua bindings

---
+++ src/bindings/MoviePlayer.tolua.h
@@ -0,0 +1,8 @@
+/*
+** Lua binding: MoviePlayer
+** Generated automatically by tolua++-1.0.92 on Sun Dec 23 22:42:38 2012.
+*/
+
+/* Exported function */
+TOLUA_API int  tolua_MoviePlayer_open (lua_State* tolua_S);
+
